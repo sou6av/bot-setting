a628c6bd7e968dcac8b8a855852b9ecaac2661d0
Akashakash3 <86599331+Akashakash3@users.noreply.github.com>
2021-07-22 15:41:26 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,10 +1,10 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2020 farhan-dqz.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
+
 WhatsAsena - Yusuf Usta
 */
-
 const Asena = require('../events');
 const Heroku = require('heroku-client');
 const Config = require('../config');
