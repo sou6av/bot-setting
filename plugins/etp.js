9e15c19dc4ded62eb5ce39d7071f4a08fb923301
Raashii <87118150+Raashii@users.noreply.github.com>
2021-08-03 08:08:30 +0530

    Update etp.js

--- plugins/etp.js
+++ plugins/etp.js
@@ -13,17 +13,17 @@ const Lang = Language.getString('ttp');
 
 if (Config.WORKTYPE == 'private') {
     Asena.addCommand({ pattern: 'etp ?(.*)', fromMe: true, desc: Lang.EMOJI_DESC }, (async (message, match) => {
-        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
+        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_EMOJI, MessageType.text);
         var uri = encodeURI(match[1])
         var ttinullimage = await axios.get('https://api.zeks.xyz/api/emoji-image?apikey=4PXD3f97yHgcO8aOD5GGIsEJB6l&emoji=' + uri, { responseType: 'arraybuffer' })
-        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.document, {filename: 'Zara.png', mimetype: Mimetype.png});
+        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by Zara' })
     }));
 }
 else if (Config.WORKTYPE == 'public') {
     Asena.addCommand({ pattern: 'etp ?(.*)', fromMe: false, desc: Lang.EMOJI_DESC }, (async (message, match) => {
-        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_WORD, MessageType.text);
+        if (match[1] === '') return await message.client.sendMessage(message.jid,Lang.NEED_EMOJI, MessageType.text);
         var uri = encodeURI(match[1])
         var ttinullimage = await axios.get('https://api.zeks.xyz/api/emoji-image?apikey=4PXD3f97yHgcO8aOD5GGIsEJB6l&emoji=' + uri, { responseType: 'arraybuffer' })
-        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.document, {filename: 'Zara.png', mimetype: Mimetype.png});
+        await message.client.sendMessage(message.jid,Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: 'Made by Zara' })
     }));
 }
