eb01d5d6c9a01f91ba06e51decdc2e89da1c5717
Raashii <87118150+Raashii@users.noreply.github.com>
2021-08-04 18:12:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
+/* Copyright (C) 2020  Yusuf Usta.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-
 WhatsAsena - Yusuf Usta
 */
 
@@ -42,6 +40,7 @@ module.exports = {
     PROMOTEMSG: process.env.PROMOTE_MESSAGE === undefined ? 'default' : process.env.PROMOTE_MESSAGE,
     DEMOTEMSG: process.env.DEMOTE_MESSAGE === undefined ? 'default' : process.env.DEMOTE_MESSAGE,
     BANMSG: process.env.BAN_MESSAGE === undefined ? 'default' : process.env.BAN_MESSAGE,
+    TAGPLK: process.env.TAG_HEADER === undefined ? 'LiyaMol Saidali' : process.env.TAG_HEADER,
     AFKMSG: process.env.AFK_MESSAGE === undefined ? 'default' : process.env.AFK_MESSAGE,
     HANDLERS: process.env.HANDLERS === undefined ? '^[.!;]' : process.env.HANDLERS,
     SEND_READ: process.env.SEND_READ === undefined ? false : convertToBool(process.env.SEND_READ),
