fd1f020734cfa7b7d23544613bce140b24051cc5
Raashii <87118150+Raashii@users.noreply.github.com>
2021-08-04 12:36:10 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -799,7 +799,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
                             }
                             else {
                                 await conn.sendMessage(conn.user.jid, '*-- ERROR REPORT [ZARA] --*' + 
-                                    '\n*LiyaMol an error has occurred!*'+
+                                    '\n*ZaraMol an error has occurred!*'+
                                     '\n*This error log may include your number or the number of an opponent. Please be careful with it!*' +
                                     '\n*You can write to our Telegram group for help.*' +
                                     '\n*Aslo you can join our support group:* https://chat.whatsapp.com/JXwRmc2lKT4IwauZnprpX5' +
