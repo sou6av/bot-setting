e0406f0b144a8fa22b08d79b4a556be1261c6c73
Raashii <87118150+Raashii@users.noreply.github.com>
2021-08-08 07:40:45 +0530

    Update ICoding.js

--- plugins/ICoding.js
+++ plugins/ICoding.js
@@ -29,7 +29,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var ttinullimage = await axios.get(`https://api.zeks.xyz/api/matrix?apikey=LVJsk3tIlDiQUa4AuncQAYe9XIp&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAP + '\n\n *----------ᴢᴀʀᴀʙᴏᴛ-----------*' })
+    await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: Config.CAP + '\n\n*----------ᴢᴀʀᴀʙᴏᴛ-----------*' })
 
   }));
 
