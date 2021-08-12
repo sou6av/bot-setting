a7fcb515d745930b22c48852ce9bd2d756f77729
souravkl11 <87891508+souravkl11@users.noreply.github.com>
2021-08-12 20:14:14 +0530

    Update gplay.js

--- plugins/gplay.js
+++ plugins/gplay.js
@@ -25,7 +25,7 @@ if (Config.WORKTYPE == 'private') {
 
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'gplay ?(.*)', fromMe: false,dontAddCommandList: true }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'gpl ?(.*)', fromMe: false,dontAddCommandList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(need);
 
