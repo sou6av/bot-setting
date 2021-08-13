303452430beee1f6223ec2470e72b90f241f8670
souravkl11 <87891508+souravkl11@users.noreply.github.com>
2021-08-13 17:20:53 +0530

    Update dropwater.js

--- plugins/dropwater.js
+++ plugins/dropwater.js
@@ -29,7 +29,7 @@ else if (Config.WORKTYPE == 'public') {
 
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/dropwater?apikey=3XRbDOZkU9hBvGdY701x59Cw6Kg&text=${encodeURIComponent(match[1])}`, { responseType: 'arraybuffer' })
 
-        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by Pikachu*' })
+        await message.sendMessage(Buffer.from(ttinullimage.data), MessageType.image, { mimetype: Mimetype.jpg, caption: '*Made by 𝐑𝐀𝐆𝐀𝐍𝐎𝐑𝐊*' })
 
     }));
     
