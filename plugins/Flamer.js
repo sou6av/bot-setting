5c99c2b4a334674379061a2e15656fb119503501
souravkl11 <87891508+souravkl11@users.noreply.github.com>
2021-08-14 21:03:38 +0530

    𝐑𝐀𝐆𝐀𝐍𝐎𝐑𝐊

--- plugins/Flamer.js
+++ plugins/Flamer.js
@@ -25,7 +25,7 @@ if (Config.WORKTYPE == 'private') {
 
 else if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'sourav ?(.*)', fromMe: false,dontAddCommandList: true }, (async (message, match) => {
+    Asena.addCommand({ pattern: 'flame ?(.*)', fromMe: false,dontAddCommandList: true }, (async (message, match) => {
 
         if (match[1] === '') return await message.sendMessage(need);
 
