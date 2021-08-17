4eba2f26749961fd11fda8551f6de2e43bc636c0
souravkl11 <87891508+souravkl11@users.noreply.github.com>
2021-08-17 08:52:57 +0530

    Add Ameer suhail Avatar

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
+  r_text[0] = "https://avatars.githubusercontent.com/u/86001869?v=4";
     
     
     var i = Math.floor(1*Math.random())
