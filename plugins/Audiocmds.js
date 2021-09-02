66e1954e0ef843c6128a547bce308d478f6c3c05
Blrx.antiban <88765751+bot-repo@users.noreply.github.com>
2021-09-02 13:38:00 +0530

    test

--- plugins/Audiocmds.js
+++ plugins/Audiocmds.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('wallpaper');
 
-Asena.addCommand({pattern: 'audio', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+Asena.addCommand({pattern: 'audio', fromMe: false, desc: 'Gives audio commands'}, (async (message, match) => {
 
     var r_text = new Array ();
     
