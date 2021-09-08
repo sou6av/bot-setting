1d05b2c1f510324b7f187e609ffd6a0a873f0143
Blrx.antiban <88765751+bot-repo@users.noreply.github.com>
2021-09-08 11:19:44 +0530

    sᴏᴜʀᴀᴠᴋʟ¹¹

--- plugins/bad.js
+++ plugins/bad.js
@@ -40,8 +40,8 @@ notheri()
 var ldc = ''
 if (Config.LANG == 'AZ') ldc = '*Bağlantı Aşkarlandı!*'
 if (Config.LANG == 'TR') ldc = '*‎Link Tespit Edildi!*'
-if (Config.LANG == 'EN') ldc = '*ഇത് ഞാനും ചെയ്യില്ലനീയും ചെയ്യില്ല* \n 😅 *arod parayan aar kelkaan* \n *edth purath aakiyathin shesham aan parayunne* 😅😅'
-if (Config.LANG == 'ML') ldc = '*ലിങ്ക് കണ്ടെത്തി!*'
+if (Config.LANG == 'EN') ldc = '*ഇത് ഞാനും ചെയ്യില്ലനീയും ചെയ്യില്ല* \n 😅 *entho parayana, ayyo ayyo 😂* \n *edth purath aakiyathin shesham aan parayunne* 😅😅'
+if (Config.LANG == 'ML') ldc = '*ഇത് ഞാനും ചെയ്യില്ലനീയും ചെയ്യില്ല* \n 😅 *entho parayana, ayyo ayyo 😂* \n *edth purath aakiyathin shesham aan parayunne* 😅😅'
 if (Config.LANG == 'ID') ldc = '*Tautan Terdeteksi!*'
 if (Config.LANG == 'PT') ldc = '*Link Detectado!*'
 if (Config.LANG == 'RU') ldc = '*Ссылка обнаружена!*'
