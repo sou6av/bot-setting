085627b7d558a93aae53757232bd50382e0e9922
Blrx.antiban <88765751+bot-repo@users.noreply.github.com>
2021-09-08 09:30:17 +0530

    GROUP LINK ITTA BAN AVILLA 😉

--- plugins/bad.js
+++ plugins/bad.js
@@ -59,12 +59,13 @@ Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (mess
         let regex2 = new RegExp('Myre')
         let regex3 = new RegExp('poorimone')
         let regex4 = new RegExp('poor')
-        let regex5 = new RegExp('https://chat.whatsapp.com/')
+        let regex5 = new RegExp('Zara')
         let regex6 = new RegExp('andi')
-        let regex7 = new RegExp('mmmmmmmmmmm')
+        let regex7 = new RegExp('Veruthe keriyatha')
         let regex8 = new RegExp('umban')
         let regex9 = new RegExp('oombi')
         let regex10 = new RegExp('Myr')
+        let regex11 = new RegExp('myre')
         if (regex1.test(message.message)) {
             var us = await checkUsAdmin(message)
             var im = await checkImAdmin(message)
@@ -144,6 +145,15 @@ Asena.addCommand({on: 'text', fromMe: false, deleteCommand: false}, (async (mess
             if (us) return;
             await message.client.groupRemove(message.jid, [message.data.participant]);         
             await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
-        }      
+        )
+         else if (regex11.test(message.message)) {
+            var us = await checkUsAdmin(message)
+            var im = await checkImAdmin(message)
+            if (!im) return;
+            if (us) return;
+            await message.client.groupRemove(message.jid, [message.data.participant]);         
+            await message.client.sendMessage(message.jid,ldc, MessageType.text, {quoted: message.data })
+              
+         }      
     }
 }));
