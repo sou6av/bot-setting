98863d79e268d460ad09a548435cd0ed383de5bc
Blrx.antiban <88765751+bot-repo@users.noreply.github.com>
2021-09-09 09:41:47 +0530

    SORRY, THE DELETE PLUGIN IS CURRENTLY UNDER MAINTAINENCE

--- plugins/delete.js
+++ plugins/delete.js
@@ -1,6 +1,6 @@
 const Asena = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
-const DEL_DESC = "Deletes The Replied Message Send By The Bot [ ✅️ Official External Plugin ]"
+const DEL_DESC = "Sorry, this plugin is currently under maintainence"
 
 Asena.addCommand({pattern: 'del', fromMe: false, desc: DEL_DESC}, (async (message, match) => {
 
