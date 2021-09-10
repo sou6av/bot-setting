1959614f766ff7b9cc767fb9ab787004cf670495
Blrx.antiban <88765751+bot-repo@users.noreply.github.com>
2021-09-10 14:24:46 +0530

    TEST X2

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -1,6 +1,7 @@
 const Asena = require('../events');
 const Config = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
+const fs = require("fs")
 
 const Language = require('../language');
 const Lang = Language.getString('_asena');
