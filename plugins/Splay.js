fbce3eeb60440550ec1c6d0ea7bb1c35e4ed14c9
Blrx.antiban <88765751+bot-repo@users.noreply.github.com>
2021-09-10 09:54:04 +0530

    Update Splay.js

--- plugins/Splay.js
+++ plugins/Splay.js
@@ -1,8 +1,3 @@
-/*coded by ameer&hisham
-youtube https://youtube.com/c/ICHUTECH
-https://youtube.com/channel/UCpGa88rhUFYj-6-LBqBbWKw
-*/
-
 const Asena = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
